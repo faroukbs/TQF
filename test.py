32d936d1848b94806a66e7b25df58912a3677c9a
faroukbs <farouk.boussaid@esprit.tn>
2023-09-30 01:35:13 +0100

    test

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 import os
 
-
-def extract()-> dict:
+def extract() -> dict:
     # extract data from api
     API_URL = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/coronavirus-commercants-parisiens-livraison-a-domicile/records?limit=90"
     response = requests.get(API_URL)
@@ -12,9 +11,8 @@ def extract()-> dict:
         data = response.json()['results']
         return data
 
-
-def transform(data:dict) -> pd.DataFrame:
-    """ Transforms the dataset into desired structure and filters"""
+def transform(data: dict) -> pd.DataFrame:
+    """ Transforms the dataset into the desired structure and filters"""
     df = pd.DataFrame(data)
     print(f"Total Number of commerces from API {len(data)}")
     df['lon'] = [foo.get('lon') for foo in df['geo_point_2d']]
@@ -25,28 +23,18 @@ def transform(data:dict) -> pd.DataFrame:
        'site_internet', 'telephone', 'mail', 
        'lon', 'lat']]
 
+def load(df: pd.DataFrame) -> None:
+    # Retrieve MySQL environment variables
+    db_user = os.environ.get('MYSQL_USER')
+    db_password = os.environ.get('MYSQL_PASSWORD')
+    db_name = os.environ.get('MYSQL_DATABASE')
 
+    """ Loads data into a MySQL database"""
+    engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@db/{db_name}')
+    # Insert the dataframe into MySQL
+    df.to_sql('commerces', engine, if_exists='replace', index=False)
 
-
-
-def load(df:pd.DataFrame)-> None:
-    """ Loads data into a sql database"""
-   # get env vars
-    db_user = os.environ['DB_USER'] 
-    db_pass = os.environ['DB_PASSWORD']
-    db_host = os.environ['DB_HOST']
-    db_name = os.environ['DB_NAME']
-
-    conn_str = f'mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}'
-
-    engine = create_engine(conn_str)
-    # Insert the dataframe into SQL Server
-    df.to_sql('commerces', engine, if_exists='replace', index=False)     # Close the database connection
-
-
-
-data = extract()
-df = transform(data)
-#print(type(str(df['nom_du_commerce'])))
-load(df)
-
+if __name__ == "__main__":
+    data = extract()
+    df = transform(data)
+    load(df)
